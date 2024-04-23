0659a39f6a3e41d6efe14ec943554a2a8bf8dd5c
Vinu Cyril <vinucyril@gmail.com>
2024-04-23 21:57:15 +0530

    removing states

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,13 @@
-import { useEffect, useState } from "react";
-import { SideBarContext } from "./contexts/SidebarContext";
-import { BoardContext } from "./contexts/BoardContext";
 import Layout from "./Layout";
-import data from "./data.json";
 import TaskContent from "./TaskContent";
 
 function App() {
-  const [board, setBoard] = useState(data.boards);
-  const [selectedIndex, setSelectedIndex] = useState(0);
-  const [sideBar, setSideBar] = useState(true);
-
   return (
     <>
       <div className="App font-jakarta">
-        <BoardContext.Provider
-          value={{ board, setBoard, selectedIndex, setSelectedIndex }}
-        >
-          <SideBarContext.Provider value={{ sideBar, setSideBar, board }}>
-            <Layout>
-              <TaskContent />
-            </Layout>
-          </SideBarContext.Provider>
-        </BoardContext.Provider>
+        <Layout>
+          <TaskContent />
+        </Layout>
       </div>
     </>
   );
