935932da66a959483f72e0cc3e4612b119ad49e0
Vinu Cyril <vinucyril@gmail.com>
2024-04-23 21:42:55 +0530

    new change

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { SideBarContext } from "./contexts/SidebarContext";
 import { BoardContext } from "./contexts/BoardContext";
 import Layout from "./Layout";
